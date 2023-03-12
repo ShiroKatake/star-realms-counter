e1d6d4fbfa983aefc8e128fc4d171bb82a9adee4
ShiroKatake <tgkhang262@gmail.com>
2023-03-12 19:02:54 +1100

    fix(increment text color): changed to black

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -3,6 +3,6 @@ export const colors = {
   damage: "#ce4e63",
   trade: "#f1e367",
   authority: "#5ece7a",
-  increase: "#00ff00",
-  decrease: "#a50000",
+  increase: "black",
+  decrease: "black",
 };
