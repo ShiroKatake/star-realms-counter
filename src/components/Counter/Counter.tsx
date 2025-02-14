d4344048808024e2f7839c01e5c331824e9dfb98
ShiroKatake <tgkhang262@gmail.com>
2025-02-15 02:12:03 +1100

    fix(long press count): was not tracking long increments

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,5 +1,11 @@
 import { WindowHeightContext } from "App";
-import React, { useContext, useReducer, useRef, useState } from "react";
+import React, {
+  useContext,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
 import { FaUndo } from "react-icons/fa";
 import {
   Button,
@@ -20,20 +26,20 @@ interface State {
 }
 
 interface Action {
-  type: "update" | "reset";
+  type: "update" | "resetCount" | "resetAmount";
   amount?: number;
-  isCounting?: boolean;
 }
 
 const reducer = (state: State, action: Action) => {
-  if (!action.isCounting) state.amountChanged = 0;
   switch (action.type) {
     case "update": {
       const newCount = state.count + (action.amount ?? 0);
       const amountChanged = state.amountChanged + (action.amount ?? 0);
       return { count: newCount, amountChanged: amountChanged };
     }
-    case "reset":
+    case "resetAmount":
+      return { count: state.count, amountChanged: 0 };
+    case "resetCount":
       return { count: 0, amountChanged: 0 };
     default:
       return { count: state.count, amountChanged: 0 };
@@ -49,15 +55,19 @@ export const Counter: React.FC<CounterProps> = ({ className, isResetting }) => {
   const fadeTimer = useRef<NodeJS.Timeout>();
   const resetChangeTimer = useRef<NodeJS.Timeout>();
 
+  useEffect(() => {
+    if (!isCounting) dispatch({ type: "resetAmount" });
+  }, [isCounting]);
+
   const resetCooldown = () => {
     setIsInitialised(true);
     setIsCounting(true);
     setIsFading(false);
     clearTimeout(fadeTimer.current);
+    clearTimeout(resetChangeTimer.current);
     fadeTimer.current = setTimeout(() => {
       setIsFading(true);
     }, 600);
-    clearTimeout(resetChangeTimer.current);
     resetChangeTimer.current = setTimeout(() => {
       setIsCounting(false);
     }, 2000);
@@ -69,22 +79,22 @@ export const Counter: React.FC<CounterProps> = ({ className, isResetting }) => {
         windowWidth={windowWidth}
         onClick={() => {
           resetCooldown();
-          dispatch({ type: "update", amount: 1, isCounting });
+          dispatch({ type: "update", amount: 1 });
         }}
         onLongPress={() => {
           resetCooldown();
-          dispatch({ type: "update", amount: 10, isCounting });
+          dispatch({ type: "update", amount: 10 });
         }}
       />
       <Button
         windowWidth={windowWidth}
         onClick={() => {
           resetCooldown();
-          dispatch({ type: "update", amount: -1, isCounting });
+          dispatch({ type: "update", amount: -1 });
         }}
         onLongPress={() => {
           resetCooldown();
-          dispatch({ type: "update", amount: -10, isCounting });
+          dispatch({ type: "update", amount: -10 });
         }}
       />
       <Value>{state.count}</Value>
@@ -97,7 +107,7 @@ export const Counter: React.FC<CounterProps> = ({ className, isResetting }) => {
       ) : null}
       <ResetButton
         isResetting={isResetting}
-        onClick={() => dispatch({ type: "reset" })}
+        onClick={() => dispatch({ type: "resetCount" })}
       >
         <FaUndo />
       </ResetButton>
